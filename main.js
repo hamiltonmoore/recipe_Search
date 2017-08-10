d4eb04b522c731d1762e729d320d3d29e2a7a2c1
hamilton moore <hamlmoor@gmail.com>
2017-08-10 17:29:01 -0500

    added an additional method to show popup

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ let resultsDisplay = document.querySelector(".boxes");
 search.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
 
-        let popupText = `<p>click an image for recipe</p>`
-        let popup = document.querySelector(".popup");
-        popup.innerHTML = popupText;
+        // let popupText = `<p>click an image for recipe</p>`
+        // let popup = document.querySelector(".popup");
+        // popup.innerHTML = popupText;
+
+        //either option ^ works to show new text!!! 
+
+        let text = document.querySelector(".popup");
+        text.textContent = "Click an Image";
 
         resultsDisplay.innerHTML = '';
         let url = "http://recipepuppyproxy.herokuapp.com/api/?q=" + search.value;
