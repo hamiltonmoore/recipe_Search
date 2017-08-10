8afc61e62bba607fe8d1b2f9038cc4c065d9bddc
hamilton moore <hamlmoor@gmail.com>
2017-08-10 11:39:47 -0500

    added href to images in js

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
 
 let search = document.getElementById("mySearch");
+let resultsDisplay = document.querySelector(".boxes");
 
 search.addEventListener("keyup", function (event) {
-    if (event.keyCode == 13) {
+    if (event.keyCode === 13) {
+        resultsDisplay.innerHTML = '';
         let url = "http://recipepuppyproxy.herokuapp.com/api/?q=" + search.value;
 
-        axios.get(url)
-            .then(function (response) {
+        axios.get(url).then(function (response) {
 
-
-                for (i = 0; i < response.data.results.length; i++) {
-                    let data = response.data.results[i];
-                    if (data.thumbnail === '') {
-                        const recipe =
-                            `<img src="fork-and-knife.jpg">`
-                        document.querySelector(".boxes").innerHTML += recipe;
-                    } else {
-                        const recipe =
-                            `<img src="${data.thumbnail}" >`
-                        document.querySelector(".boxes").innerHTML += recipe;
-                    }
+            for (i = 0; i < response.data.results.length; i++) {
+                let data = response.data.results[i];
+                if (data.thumbnail === '') {
+                    console.log(`no image for item ${[i + 1]} the ${data.title}`);
+                } else {
+                    const recipe =
+                        `<img class="img" src="${data.thumbnail}" id="${data.href}">`
+                    document.querySelector(".boxes").innerHTML += recipe;
                 }
-            });
+            }
+        });
     }
 });
+
+resultsDisplay.addEventListener("click", function (e) {
+    console.log('doing something');
+    let link = e.target.getAttribute("id");
+    if (link) {
+        open(link);
+    } else {
+        return;
+    }
+});
+
+
+
 
 // let url = `http://recipepuppyproxy.herokuapp.com/api/?q=${search.value}`;  Works the same as line 6
 
